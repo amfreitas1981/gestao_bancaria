9c69f50dfb3f4e1b67894b5cea981a7e4beefdaf
Alexandre Freitas <alexandremfreitas@gmail.com>
2024-06-13 13:17:21 -0300

    Initial version

---
+++ src/test/java/com/banking/account/transact/TransactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.account.transact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
